c28f0fab75f1d82bca4837a28d552fbd2960848a
zzzzzzzzzzzzzzzzzzzzl <111021919+zzzzzzzzzzzzzzzzzzzzl@users.noreply.github.com>
2022-09-15 14:31:51 +1200

    Create test1.4.1.py
    not actualy a python file. just some notes for the bone class

---
+++ test1.4.1.py
@@ -0,0 +1,40 @@
+how do we have 2 falling points which maintain a constant distance?
+
+
+we have 2 points a1,b1 and bone which is the distance between them
+and a2,b2 which is what we will have after we transform bone 
+
+every frame we need to rotate the  bone(M) so that it maintains a constant length as a and b change y value
+
+we can find what rotation we need for bone with =(M^2)-(y^2)
+this will give us a2,b2
+
+distrubution:
+disturubution will be absed off of velocty 
+the sum of the distrubution will be 1: v1+v2=s  vm1=v1/s  vm2=v2/s
+
+
+
+to get the average we then go (a1[x]+b1[x])/2=mid1)
+and do the same for a2 b2 (a2[x]+b2[x])/2=mid2)
+
+mid2=mid1
+mid1=((diff/2)*vm1)+m2+((diff/2)*vm2)
+this will give us our x shift
+
+to handle the y pull for when our 2 points are outside of each others range 
+the dumb way we could do it would be to give both points an equall velocity
+the smart way idk this dumb way would be fine if not ideal if we had a way to do velocity exchange
+
+its fine whatever.we can shiftnkmnmn
+if v
+
+
+
+wgjaghwajhwaghwah
+WHAT IF THE Y ARE OUTSIDE OF EACH OTHERS RANGE
+we need to get pull the y axis and we can use total velocicty to determine the distrubution.
+
+asdhaskjsfahsfajk
+
+
